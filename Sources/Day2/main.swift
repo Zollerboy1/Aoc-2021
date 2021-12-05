8b38d1e0f60bf824f6ebcd3edefa34476a4d0219
Josef Zoller <josef@walterzollerpiano.com>
2021-12-05 07:39:22 +0100

    Add solution for day 2

--- Sources/Day2/main.swift
+++ Sources/Day2/main.swift
@@ -3,5 +3,27 @@ import Foundation
 let url = Bundle.module.url(forResource: "day2", withExtension: "txt")!
 let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
+var horizontal = 0
+var depth = 0
+var aim = 0
 
-let lines = fileContents.split(separator: "\n")
+for line in fileContents.split(separator: "\n") {
+    let split = line.split(separator: " ")
+    
+    let command = split[0]
+    let number = Int(split[1])!
+    
+    switch command {
+    case "forward":
+        horizontal += number
+        depth += aim * number
+    case "up":
+        aim -= number
+    case "down":
+        aim += number
+    default:
+        fatalError()
+    }
+}
+
+print(horizontal * depth)
