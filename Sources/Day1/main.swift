036f09b55e610942732c60bc903ec18921f143be
Josef Zoller <josef@walterzollerpiano.com>
2021-12-05 07:39:07 +0100

    Add solution for day 1

--- Sources/Day1/main.swift
+++ Sources/Day1/main.swift
@@ -4,4 +4,5 @@ let url = Bundle.module.url(forResource: "day1", withExtension: "txt")!
 let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
-let lines = fileContents.split(separator: "\n")
+let lines = fileContents.split(separator: "\n").compactMap { Int($0) }
+print(zip(lines, lines[3...]).reduce(0, { $0 + ($1.0 < $1.1 ? 1 : 0) }))
