d4cb05e805702a7892dc61a4422800c65f2c0ac9
Josef Zoller <josef@walterzollerpiano.com>
2021-12-05 07:39:28 +0100

    Add solution for day 3

--- Sources/Day3/main.swift
+++ Sources/Day3/main.swift
@@ -3,5 +3,28 @@ import Foundation
 let url = Bundle.module.url(forResource: "day3", withExtension: "txt")!
 let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
+let lines = fileContents.split(separator: "\n").compactMap { Int($0, radix: 2) }
 
-let lines = fileContents.split(separator: "\n")
+let lineLength = 12
+
+var oxygenGeneratorNumbers = lines
+var cO2ScrubberNumbers = lines
+
+for i in 0..<lineLength {
+    if oxygenGeneratorNumbers.count > 1 {
+        let count = Double(oxygenGeneratorNumbers.count)
+        let bitCount = oxygenGeneratorNumbers.reduce(0, {
+            $0 + ($1 & (1 << (lineLength - 1 - i)) == 0 ? 0 : 1)
+        })
+        oxygenGeneratorNumbers = oxygenGeneratorNumbers.filter { Double(bitCount) > count / 2 ? ($0 & (1 << (lineLength - 1 - i)) != 0) : ($0 & (1 << (lineLength - 1 - i)) == 0) }
+    }
+    if cO2ScrubberNumbers.count > 1 {
+        let count = Double(cO2ScrubberNumbers.count)
+        let bitCount = cO2ScrubberNumbers.reduce(0, { $0 + ($1 & (1 << (lineLength - 1 - i)) == 0 ? 0 : 1) })
+        cO2ScrubberNumbers = cO2ScrubberNumbers.filter { Double(bitCount) < count / 2 ? ($0 & (1 << (lineLength - 1 - i)) != 0) : ($0 & (1 << (lineLength - 1 - i)) == 0) }
+    }
+}
+
+print((oxygenGeneratorNumbers.first! + 1) * cO2ScrubberNumbers.first!)
+//                                   ^^^
+//                                  Here
