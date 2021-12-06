6d96158097ddcbf64f3af99c100ae6fcd6929957
Josef Zoller <josef@walterzollerpiano.com>
2021-12-06 06:29:36 +0100

    Add solution for day 6

--- Sources/Day6/main.swift
+++ Sources/Day6/main.swift
@@ -5,3 +5,26 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+let initialAges = lines.first!.split(separator: ",").compactMap { Int($0) }
+
+
+var ageCounts = initialAges.reduce(into: [Int](repeating: 0, count: 9)) { $0[$1] += 1 }
+for _ in 0..<256 {
+    var newAgeCounts = [Int](repeating: 0, count: 9)
+    
+    for (i, ageCount) in ageCounts.enumerated() {
+        if i == 0 {
+            newAgeCounts[8] += ageCount
+            newAgeCounts[6] += ageCount
+        } else {
+            newAgeCounts[i - 1] += ageCount
+        }
+    }
+    
+    ageCounts = newAgeCounts
+    
+    
+}
+
+print(ageCounts.reduce(0) { $0 + $1 })
