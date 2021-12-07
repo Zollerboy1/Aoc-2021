95a02d22289a8aaceaf915658121ee029f506a63
Josef Zoller <josef@walterzollerpiano.com>
2021-12-07 13:49:52 +0100

    Add solution for day 7

--- Sources/Day7/main.swift
+++ Sources/Day7/main.swift
@@ -5,3 +5,80 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+let crabs = lines.first!.split(separator: ",").compactMap { Int($0) }
+
+
+let sum = crabs.reduce(0) { $0 + $1 }
+
+let mean = sum / crabs.count
+
+var fuelCost = 0
+for crab in crabs {
+    for j in 0..<Int((crab - mean).magnitude) {
+        fuelCost += j + 1
+    }
+}
+
+var meanFuelCost = fuelCost
+
+
+fuelCost = 0
+for crab in crabs {
+    for j in 0..<Int((crab - (mean - 1)).magnitude) {
+        fuelCost += j + 1
+    }
+}
+
+var lowerFuelCost = fuelCost
+
+
+fuelCost = 0
+for crab in crabs {
+    for j in 0..<Int((crab - (mean + 1)).magnitude) {
+        fuelCost += j + 1
+    }
+}
+
+var higherFuelCost = fuelCost
+
+var lastFuelCost: Int
+if (lowerFuelCost < meanFuelCost) {
+    var i = mean - 1
+    
+    lastFuelCost = meanFuelCost
+    var currentFuelCost = lowerFuelCost
+    
+    while currentFuelCost < lastFuelCost {
+        var fuelCost = 0
+        for crab in crabs {
+            for j in 0..<Int((crab - i).magnitude) {
+                fuelCost += j + 1
+            }
+        }
+        
+        lastFuelCost = currentFuelCost
+        currentFuelCost = fuelCost
+        i -= 1
+    }
+} else {
+    var i = mean + 1
+    
+    lastFuelCost = meanFuelCost
+    var currentFuelCost = higherFuelCost
+    
+    while currentFuelCost < lastFuelCost {
+        var fuelCost = 0
+        for crab in crabs {
+            for j in 0..<Int((crab - i).magnitude) {
+                fuelCost += j + 1
+            }
+        }
+        
+        lastFuelCost = currentFuelCost
+        currentFuelCost = fuelCost
+        i += 1
+    }
+}
+
+print(lastFuelCost)
